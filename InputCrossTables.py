0ec91033d5aafe53bea78caf0b7a3b5ff89ca57c
Usama Athar <41828100+athar-usama@users.noreply.github.com>
2024-03-19 19:44:30 +0500

    Updated Files For Processing Input Data

--- InputCrossTables.py
+++ InputCrossTables.py
@@ -50,9 +50,9 @@ def getdictionary(df, area):
 
 def aggregate_age(data):
     # Define age brackets
-    child_ages = ['0-4', '5-7', '8-9', '10-14', '15', '16-17', '18-19']
-    adult_ages = ['20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64']
-    elder_ages = ['65-69', '70-74', '75-79', '80-84', '85+']
+    child_ages = ['0_4', '5_7', '8_9', '10_14', '15', '16_17', '18_19']
+    adult_ages = ['20_24', '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59', '60_64']
+    elder_ages = ['65_69', '70_74', '75_79', '80_84', '85+']
 
     aggregate = {}
     for key in data.keys():
@@ -95,9 +95,9 @@ def generate_combinations(left, right, total):
 
 
 def convert_marital_cross_table(old_cross_table):
-    new_age_categories = ['0-4', '5-7', '8-9', '10-14', '15', '16-17', '18-19', '20-24',
-                          '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59',
-                          '60-64', '65-69', '70-74', '75-79', '80-84', '85+']
+    new_age_categories = ['0_4', '5_7', '8_9', '10_14', '15', '16_17', '18_19', '20_24',
+                          '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59',
+                          '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
     marital_statuses = ['Single', 'Married', 'Partner', 'Separated', 'Divorced', 'Widowed']
 
     # Initialize new cross table with all zeros
@@ -108,22 +108,17 @@ def convert_marital_cross_table(old_cross_table):
         parts = key.split(' ')
         gender, age_range, marital_status = parts[0], parts[1], ' '.join(parts[2:])
 
-        # For age below 16, count them as 'Single', rest as '0'
-        if age_range in ['0-4', '5-7', '8-9', '10-14', '15']:
-            new_key = f'{gender} 15 Single'  # All below 16 are considered 15 and single
+        new_key = f'{gender} {age_range} {marital_status}'
+        if new_key in new_cross_table:
             new_cross_table[new_key] += value
-        else:
-            new_key = f'{gender} {age_range} {marital_status}'
-            if new_key in new_cross_table:
-                new_cross_table[new_key] += value
 
     return new_cross_table
 
 
 def convert_qualification_cross_table(old_cross_table):
-    new_age_categories = ['0-4', '5-7', '8-9', '10-14', '15', '16-17', '18-19', '20-24',
-                          '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59',
-                          '60-64', '65-69', '70-74', '75-79', '80-84', '85+']
+    new_age_categories = ['0_4', '5_7', '8_9', '10_14', '15', '16_17', '18_19', '20_24',
+                          '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59',
+                          '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
     qualification_levels = ['no', 'level1', 'level2', 'apprent', 'level3', 'level4+', 'other']
     genders = ['M', 'F']
 
@@ -136,11 +131,16 @@ def convert_qualification_cross_table(old_cross_table):
 
     # Map the old age ranges to the new age categories
     age_range_mapping = {
-        '16-24': ['16-17', '18-19', '20-24'],
-        '25-34': ['25-29', '30-34'],
-        '35-49': ['35-39', '40-44', '45-49'],
-        '50-64': ['50-54', '55-59', '60-64'],
-        '65+': ['65-69', '70-74', '75-79', '80-84', '85+']
+        '0_4': ['0_4'],
+        '5_7': ['5_7'],
+        '8_9': ['8_9'],
+        '10_14': ['10_14'],
+        '15': ['15'],
+        '16_24': ['16_17', '18_19', '20_24'],
+        '25_34': ['25_29', '30_34'],
+        '35_49': ['35_39', '40_44', '45_49'],
+        '50_64': ['50_54', '55_59', '60_64'],
+        '65+': ['65_69', '70_74', '75_79', '80_84', '85+']
     }
 
     # Distribute counts uniformly
@@ -158,11 +158,49 @@ def convert_qualification_cross_table(old_cross_table):
 
     return new_cross_table
 
+def convert_composition_sex_age_cross_table(old_cross_table):
+    new_age_categories = ['0_4', '5_7', '8_9', '10_14', '15', '16_17', '18_19', '20_24',
+                          '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59',
+                          '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
+    compositions = ['SP-Elder', 'SP-Adult', 'OF-Elder', 'OF-Married', 'OF-Married-0C', 'OF-Married-1C', 'OF-Married-2C', 'OF-Married-ND', 'OF-Cohabiting', 'OF-Cohabiting-0C', 'OF-Cohabiting-1C', 'OF-Cohabiting-2C', 'OF-Cohabiting-ND', 'OF-LoneParent', 'OF-Lone-1C', 'OF-Lone-2C', 'OF-Lone-ND', 'OH-1C', 'OH-2C', 'OH-Student', 'OH-Elder', 'OH-Adult']
+    genders = ['M', 'F']
+
+    # Initialize new cross table with all zeros
+    new_cross_table = {}
+    for gender in genders:
+        for age in new_age_categories:
+            for composition in compositions:
+                new_cross_table[f'{gender} {age} {composition}'] = 0
+
+    # Map the old age ranges to the new age categories
+    age_range_mapping = {
+        '0_15': ['0_4', '5_7', '8_9', '10_14', '15'],
+        '16_24': ['16_17', '18_19', '20_24'],
+        '25_34': ['25_29', '30_34'],
+        '35_49': ['35_39', '40_44', '45_49'],
+        '50+': ['50_54', '55_59', '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
+    }
+
+    # Distribute counts uniformly
+    for key, value in old_cross_table.items():
+        parts = key.split(' ')
+        gender, old_age_range, comp = parts[0], parts[1], parts[2]
+
+        if old_age_range in age_range_mapping:
+            new_ages = age_range_mapping[old_age_range]
+            count_per_age = round(value / len(new_ages))  # Uniform distribution
+
+            for new_age in new_ages:
+                new_key = f'{gender} {new_age} {comp}'
+                new_cross_table[new_key] += count_per_age
+
+    return new_cross_table
+
 
 def convert_household_cross_table(old_cross_table):
-    new_age_categories = ['0-4', '5-7', '8-9', '10-14', '15', '16-17', '18-19', '20-24',
-                          '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59',
-                          '60-64', '65-69', '70-74', '75-79', '80-84', '85+']
+    new_age_categories = ['0_4', '5_7', '8_9', '10_14', '15', '16_17', '18_19', '20_24',
+                          '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59',
+                          '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
     household_compositions = ['1E_0C', '1A_0C', '2E_0C', '3A_1C', '3A_0C', '2A_0C', '2A_1C', '2A_3C', '1A_1C']
     genders = ['M', 'F']
 
@@ -175,11 +213,11 @@ def convert_household_cross_table(old_cross_table):
 
     # Map the old age ranges to the new age categories
     age_range_mapping = {
-        '0-15': ['0-4', '5-7', '8-9', '10-14', '15'],
-        '16-24': ['16-17', '18-19', '20-24'],
-        '25-34': ['25-29', '30-34'],
-        '35-49': ['35-39', '40-44', '45-49'],
-        '50+': ['50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '85+']
+        '0_15': ['0_4', '5_7', '8_9', '10_14', '15'],
+        '16_24': ['16_17', '18_19', '20_24'],
+        '25_34': ['25_29', '30_34'],
+        '35_49': ['35_39', '40_44', '45_49'],
+        '50+': ['50_54', '55_59', '60_64', '65_69', '70_74', '75_79', '80_84', '85+']
     }
 
     # Distribute counts uniformly
@@ -197,45 +235,105 @@ def convert_household_cross_table(old_cross_table):
 
     return new_cross_table
 
-path = os.path.join(os.path.dirname(os.getcwd()), 'Diff-SynPoP')
+path = os.path.join(os.path.dirname(os.getcwd()), 'input/diffspop/Diff-SynPoP')
 
 sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'sex_by_age_5yrs.csv'))
 cols = [(col.split(' ')[1] + ' ' + col.split(' ')[0]) for col in sex_by_age.columns[2:]]
 sex_by_age.columns = ['geography code', 'total'] + cols
 
+substring_mapping_rel = {
+    'OR': 'O',
+    'NR': 'N'
+}
+
 religion_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'religion_by_sex_by_age.csv'))
+for col in religion_by_sex_by_age.columns:
+    for old_substring, new_substring in substring_mapping_rel.items():
+        if old_substring in col:
+            new_col = col.replace(old_substring, new_substring)
+            religion_by_sex_by_age.rename(columns={col: new_col}, inplace=True)
+            break
 religion_by_sex_by_age = religion_by_sex_by_age.drop(columns=[col for col in religion_by_sex_by_age.columns if 'All' in col])
 
 ethnicities = ['W0', 'M0', 'B0', 'A0', 'O0']
 ethnic_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'ethnic_by_sex_by_age.csv'))
 
 ethnic_by_religion = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'ethnic_by_religion.csv'))
-ethnic_by_religion = ethnic_by_religion.drop(columns=[col for col in ethnic_by_religion.columns[2:] if  col.split(' ')[0] in ethnicities])
-# ethnic_by_religion.columns = [col.replace('0', '') for col in ethnic_by_religion.columns]
+ethnic_by_religion = ethnic_by_religion.drop(columns=[col for col in ethnic_by_religion.columns[2:] if  col.split(' ')[0] not in ethnicities])
+ethnic_by_religion.columns = [col.replace('0', '') for col in ethnic_by_religion.columns]
 
 marital_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'marital_by_sex_by_age.csv'))
 # print(convert_marital_cross_table(getdictionary(marital_by_sex_by_age, 'E02005924')))
 
 qualification_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'qualification_by_sex_by_age.csv'))
 # print(convert_qualification_cross_table(getdictionary(qualification_by_sex_by_age, 'E02005924')))
-HH_composition_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'HH_composition_by_sex_by_age.csv'))
 
-drop_cols = ['SP', 'OF', 'OH']
+# new terms for household compositions
+substring_mapping = {
+    'SP-Elder': '1PE',
+    'SP-Adult': '1PA',
+    'OF-Elder': '1FE',
+    'OF-Married-0C': '1FM-0C',
+    'OF-Married-2C': '1FM-2C',
+    'OF-Married-ND': '1FM-nA',
+    'OF-Cohabiting-0C': '1FC-0C',
+    'OF-Cohabiting-2C': '1FC-2C',
+    'OF-Cohabiting-ND': '1FC-nA',
+    'OF-Lone-2C': '1FL-2C',
+    'OF-Lone-ND': '1FL-nA',
+    'OH-2C': '1H-2C',
+    'OH-Student': '1H-nS',
+    'OH-Elder': '1H-nE',
+    'OH-Adult': '1H-nA',
+}
+# removing aggregated columns
+substrings_to_exclude = ['OF-Married', 'OF-Cohabiting', 'OF-LoneParent']
+# removing 0 in front of aggregated ethnicities
+ethnicity_terms_mapping = {
+    'W0': 'W',
+    'M0': 'M',
+    'A0': 'A',
+    'B0': 'B',
+    'O0': 'O'
+}
+
+# processing household composition by ethnicity cross table
 HH_composition_by_Ethnicity = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'HH_composition_by_ethnicity.csv'))
-HH_composition_by_Ethnicity = HH_composition_by_Ethnicity.drop(columns=[col for col in HH_composition_by_Ethnicity.columns[2:] if  col.split(' ')[1] in ethnicities])
-HH_composition_by_Ethnicity = HH_composition_by_Ethnicity.drop(columns=[col for col in HH_composition_by_Ethnicity.columns[2:] if  col.split(' ')[1] in ethnicities])
-HH_composition_by_Ethnicity = HH_composition_by_Ethnicity.drop(columns=[col for col in HH_composition_by_Ethnicity.columns[2:] if  col.split(' ')[0] in drop_cols])
-# HH_composition_by_Ethnicity.columns = [col.replace('0', '') for col in HH_composition_by_Ethnicity.columns]
+filtered_columns = [col for col in HH_composition_by_Ethnicity.columns if any(substring in col for substring in ['geography code', 'total', 'W0', 'M0', 'A0', 'B0', 'O0'])]
+HH_composition_by_Ethnicity = HH_composition_by_Ethnicity[filtered_columns]
+for col in HH_composition_by_Ethnicity.columns:
+    for old_substring, new_substring in substring_mapping.items():
+        if old_substring in col:
+            new_col = col.replace(old_substring, new_substring)
+            HH_composition_by_Ethnicity.rename(columns={col: new_col}, inplace=True)
+            break
+for col in HH_composition_by_Ethnicity.columns:
+    for old_substring, new_substring in ethnicity_terms_mapping.items():
+        if old_substring in col:
+            new_col = col.replace(old_substring, new_substring)
+            HH_composition_by_Ethnicity.rename(columns={col: new_col}, inplace=True)
+            break
+filtered_columns = [col for col in HH_composition_by_Ethnicity.columns if not any(substring in col for substring in substrings_to_exclude)]
+HH_composition_by_Ethnicity = HH_composition_by_Ethnicity[filtered_columns]
+
+# processing household composition by religion cross table
 HH_composition_by_Religion = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'HH_composition_by_religion.csv'))
+for col in HH_composition_by_Religion.columns:
+    for old_substring, new_substring in substring_mapping.items():
+        if old_substring in col:
+            new_col = col.replace(old_substring, new_substring)
+            HH_composition_by_Religion.rename(columns={col: new_col}, inplace=True)
+            break
+filtered_columns = [col for col in HH_composition_by_Religion.columns if not any(substring in col for substring in substrings_to_exclude)]
+HH_composition_by_Religion = HH_composition_by_Religion[filtered_columns]
 
-
-# sex_by_age = sex_by_age[sex_by_age['geography code'] == 'E02005927']
-# ethnic_by_religion = ethnic_by_religion[ethnic_by_religion['geography code'] == 'E02005927']
-# ethnic_by_sex_by_age = ethnic_by_sex_by_age[ethnic_by_sex_by_age['geography code'] == 'E02005927']
-# religion_by_sex_by_age = religion_by_sex_by_age[religion_by_sex_by_age['geography code'] == 'E02005927']
-#
-# # save to csv
-# sex_by_age.to_csv('sex_by_age.csv', index=False)
-# ethnic_by_religion.to_csv('ethnic_by_religion.csv', index=False)
-# ethnic_by_sex_by_age.to_csv('ethnic_by_sex_by_age.csv', index=False)
-# religion_by_sex_by_age.to_csv('religion_by_sex_by_age.csv', index=False)
+# processing household composition by sex by age cross table
+HH_composition_by_sex_by_age = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'crosstables', 'HH_composition_by_sex_by_age.csv'))
+for col in HH_composition_by_sex_by_age.columns:
+    for old_substring, new_substring in substring_mapping.items():
+        if old_substring in col:
+            new_col = col.replace(old_substring, new_substring)
+            HH_composition_by_sex_by_age.rename(columns={col: new_col}, inplace=True)
+            break
+filtered_columns = [col for col in HH_composition_by_sex_by_age.columns if not any(substring in col for substring in substrings_to_exclude)]
+HH_composition_by_sex_by_age = HH_composition_by_sex_by_age[filtered_columns]
--- InputData.py
+++ InputData.py
@@ -22,6 +22,19 @@ def getdictionary(df, area):
     # return dict(sorted(dic.items()))
     return dic
 
+def getHHcomdictionary(df, area):
+    #filter df by area
+    df = df[df['geography code'] == area]
+    df.drop(['total', '1FM', '1FC', '1FL'], axis=1, inplace=True)
+    dic = {}
+    for index, row in df.iterrows():
+        for index, column in enumerate(df.columns):
+            if index==0:
+                continue
+            dic[column] = int(row[column])
+    # return dict(sorted(dic.items()))
+    return dic
+
 def get_weighted_sample(df):
     groups = list(df.columns)[2:] #drop first two columns: areacode and total
     values = df.values.flatten().tolist()[1:]
@@ -50,6 +63,12 @@ def get_total(df, area):
     total = values.pop(0)
     return total
 
+def get_HH_com_total(df, area):
+    #filter df by area
+    df = df[df['geography code'] == area]
+    df.drop(['geography code', 'total', '1FM', '1FC', '1FL'], axis=1, inplace=True)
+    total = int(df.iloc[0].sum())
+    return total
 
 def get_weighted_samples(df, size):
     groups = [col.strip() for col in list(df.columns)][2:] #drop first two columns: areacode and total
@@ -73,9 +92,9 @@ def get_weighted_samples_by_age_sex(df, age, sex, size):
 def aggregate_age_groups(age_distribution):
     # Define the age categories
     categories = {
-        'child': ['0-4', '5-7', '8-9', '10-14', '15', '16-17'],
-        'adult': ['18-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64'],
-        'elder': ['65-69', '70-74', '75-79', '80-84', '85+']
+        'child': ['0_4', '5_7', '8_9', '10_14', '15', '16_17'],
+        'adult': ['18_19', '20_24', '25_29', '30_34', '35_39', '40_44', '45_49', '50_54', '55_59', '60_64'],
+        'elder': ['65_69', '70_74', '75_79', '80_84', '85+']
     }
 
     # Initialize aggregated counts
@@ -87,7 +106,7 @@ def aggregate_age_groups(age_distribution):
             aggregated_counts[category] += age_distribution.get(age_range, 0)
     return aggregated_counts
 
-path = os.path.join(os.path.dirname(os.getcwd()), 'Diff-SynPoP')
+path = os.path.join(os.path.dirname(os.getcwd()), 'input/diffspop/Diff-SynPoP')
 
 oxford_areas = ['E02005921', 'E02005922', 'E02005923', 'E02005924', 'E02005925', 'E02005926', 'E02005927', 'E02005928', 'E02005929', 'E02005930', 'E02005931', 'E02005932', 'E02005933', 'E02005934', 'E02005935', 'E02005936', 'E02005937', 'E02005938', 'E02005939', 'E02005940', 'E02005941', 'E02005942', 'E02005943', 'E02005944', 'E02005945', 'E02005946', 'E02005947', 'E02005948', 'E02005949', 'E02005950', 'E02005951', 'E02005952', 'E02005953', 'E02005954', 'E02005955', 'E02005956', 'E02005957', 'E02005958', 'E02005959', 'E02005960', 'E02005961', 'E02005962', 'E02005963', 'E02005964', 'E02005965', 'E02005966', 'E02005967', 'E02005968', 'E02005969', 'E02005970', 'E02005971', 'E02005972', 'E02005973', 'E02005974', 'E02005975', 'E02005976', 'E02005977', 'E02005978', 'E02005979', 'E02005980', 'E02005981', 'E02005982', 'E02005983', 'E02005984', 'E02005985', 'E02005986', 'E02005987', 'E02005988', 'E02005991', 'E02005992', 'E02006886', 'E02005993', 'E02005994', 'E02005995', 'E02005996', 'E02005997', 'E02005998', 'E02005999', 'E02006000', 'E02006001', 'E02006002', 'E02006003', 'E02006004', 'E02006005', 'E02006006', 'E02006007']
 # Read census data
@@ -99,15 +118,11 @@ sexdf = sexdf[sexdf['geography code'].isin(oxford_areas)]
 
 ethnicdf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual', 'Ethnic.csv'))
 ethnicdf = ethnicdf[ethnicdf['geography code'].isin(oxford_areas)]
-ethnicdf = ethnicdf.drop(columns=[col for col in ethnicdf.columns[2:] if '0' in col]) #remove all category columns
-# ethnicdf.columns = [col.replace('0', '') for col in ethnicdf.columns]
-
+ethnicdf = ethnicdf.drop(columns=[col for col in ethnicdf.columns[2:] if '0' not in col]) #remove all category columns
+ethnicdf.columns = [col.replace('0', '') for col in ethnicdf.columns]
 
 religiondf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual', 'Religion.csv'))
 religiondf = religiondf[religiondf['geography code'].isin(oxford_areas)]
-#raname columns
-religiondf = religiondf.rename(columns={'N': 'NR', 'O': 'OR'})
-
 
 maritaldf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual', 'Marital.csv'))
 maritaldf = maritaldf[maritaldf['geography code'].isin(oxford_areas)]
@@ -115,19 +130,16 @@ maritaldf = maritaldf[maritaldf['geography code'].isin(oxford_areas)]
 qualdf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual', 'Qualification.csv'))
 qualdf = qualdf[qualdf['geography code'].isin(oxford_areas)]
 
+# processing household size distribution
 HHsizedf = pd.read_csv(os.path.join(path,  'Census_2011_MSOA', 'individual', 'HH_size.csv'))
+HHsizedf = HHsizedf.rename(columns={'8+': '8'})
 
+# processing household composition distribution
 HHcomdf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual', 'HH_Compositions.csv'))
+HHcomdf['1FM-2C'] = HHcomdf['1FM-1C'] + HHcomdf['1FM-nC']
+HHcomdf['1FC-2C'] = HHcomdf['1FC-1C'] + HHcomdf['1FC-nC']
+HHcomdf['1FL-2C'] = HHcomdf['1FL-1C'] + HHcomdf['1FL-nC']
+HHcomdf['1H-2C'] = HHcomdf['1H-1C'] + HHcomdf['1H-nC']
+HHcomdf.drop(columns=['1FM-1C', '1FM-nC', '1FC-1C', '1FC-nC', '1FL-1C', '1FL-nC', '1H-1C', '1H-nC'], inplace=True)
 
 HHtypedf = pd.read_csv(os.path.join(path, 'Census_2011_MSOA', 'individual','HH_type.csv'))
-
-# age5ydf = age5ydf[age5ydf['geography code']=='E02005927']
-# sexdf = sexdf[sexdf['geography code']=='E02005927']
-# ethnicdf = ethnicdf[ethnicdf['geography code']=='E02005927']
-# religiondf = religiondf[religiondf['geography code']=='E02005927']
-#
-# #save to csv
-# age5ydf.to_csv('age5ydf.csv', index=False)
-# sexdf.to_csv('sexdf.csv', index=False)
-# ethnicdf.to_csv('ethnicdf.csv', index=False)
-# religiondf.to_csv('religiondf.csv', index=False)
